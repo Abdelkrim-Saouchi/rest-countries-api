81e9655a3d00c366a09c8375f1b91dbd587caed5
Krimothiazine <krimouv1995@gmail.com>
2023-08-19 20:06:38 +0100

    Create custom dropdow menu for filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,17 +1,57 @@
-const Filter = () => {
-  return (
-    // <div>
-    //     <div>Filter by Region</div>
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import styles from './Filter.module.scss';
 
-    // </div>
-    <select name="region" id="region">
-      <option disabled>Filter by Region</option>
-      <option value="Africa">Africa</option>
-      <option value="America">America</option>
-      <option value="Asia">Asia</option>
-      <option value="Europe">Europe</option>
-      <option value="Oceania">Oceania</option>
-    </select>
+const Filter = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const options = [
+    { value: 'africa', label: 'Africa' },
+    { value: 'america', label: 'America' },
+    { value: 'asia', label: 'Asia' },
+    { value: 'europe', label: 'Europe' },
+    { value: 'oceania', label: 'Oceania' },
+  ];
+
+  const handleShowMenu = (e) => {
+    e.stopPropagation();
+    setShowMenu((prev) => !prev);
+  };
+
+  const selectOption = (e) => {
+    setSelectedOption(e.target.textContent);
+  };
+
+  useEffect(() => {
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    window.addEventListener('click', closeMenu);
+
+    return () => window.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
+  return (
+    <>
+      <div className={styles.dropDownContainer}>
+        <div className={styles.dropDownLabel} onClick={handleShowMenu}>
+          <div>{selectedOption || 'Filter by Region'}</div>
+          <FontAwesomeIcon icon={faAngleDown} />
+        </div>
+        {showMenu && (
+          <ul className={styles.dropDownMenu}>
+            {options.map((option) => (
+              <li key={option.value} onClick={selectOption}>
+                {option.label}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 };
 
