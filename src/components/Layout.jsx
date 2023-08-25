7f30f719974615086e2ab5516d2424ee19b72ac3
Krimothiazine <krimouv1995@gmail.com>
2023-08-25 16:23:14 +0100

    Implement toggleTheme function and fix some css style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,20 @@
 import { faMoon } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from './Layout.module.scss';
 
 const Layout = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
   return (
-    <div className="light">
+    <div className={`${theme}`}>
       <header className={styles.header}>
         <h1>Where is the world?</h1>
-        <button>
+        <button onClick={toggleTheme}>
           <FontAwesomeIcon icon={faMoon} /> Dark Mode
         </button>
       </header>
